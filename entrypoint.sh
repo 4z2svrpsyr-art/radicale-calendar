31a06fce21643aa78dd2e1e8a14152840cb5857c
4z2svrpsyr-art <4z2svrpsyr@privaterelay.appleid.com>
2026-09-08 21:54:59 +0800

    fix: simplify

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 set -e
-
-# Create default user from env vars
-USER="${RADICALE_USER:-johnathan}"
-PASS="${RADICALE_PASS:-changeme}"
-
 python3 -c "
 from passlib.hash import bcrypt
 import os
-h = bcrypt.hash(os.environ.get('RADICALE_PASS', 'changeme'))
-with open('/etc/radicale/users', 'w') as f:
-    f.write(f\"${os.environ.get('RADICALE_USER', 'johnathan')}:{h}\n\")
-print(f'User ${os.environ.get(\"RADICALE_USER\", \"johnathan\")} created')
+h = bcrypt.hash(os.environ.get(\"RADICALE_PASS\", \"changeme\"))
+with open(\"/etc/radicale/users\", \"w\") as f:
+    f.write(f\"${os.environ.get(\"RADICALE_USER\", \"johnathan\")}:{h}\n\")
+print(\"User created\")
 "
-
 exec radicale --config /etc/radicale/config
