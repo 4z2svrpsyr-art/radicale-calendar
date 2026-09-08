51be2b36d465bbc3f4a673c6e708e0fd43fff104
4z2svrpsyr-art <4z2svrpsyr@privaterelay.appleid.com>
2026-09-08 23:11:59 +0800

    cleanup

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-echo ">>> Creating user ${RADICALE_USER:-johnathan}..."
-echo "${RADICALE_USER:-johnathan}:${RADICALE_PASS:-changeme}" > /etc/radicale/users
-echo ">>> User file: $(cat /etc/radicale/users)"
-echo ">>> Config port: $(grep hosts /etc/radicale/config)"
-echo ">>> Starting Radicale..."
-exec radicale --config /etc/radicale/config --debug 2>&1
