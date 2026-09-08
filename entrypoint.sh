eba820620ac31c89a02251d0ed860d72b0e11f1d
4z2svrpsyr-art <4z2svrpsyr@privaterelay.appleid.com>
2026-09-08 22:27:29 +0800

    fix: plain auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 set -e
-python3 -c "
-import hashlib, os, base64
-pw = os.environ.get(\"RADICALE_PASS\", \"changeme\")
-user = os.environ.get(\"RADICALE_USER\", \"johnathan\")
-salt = os.urandom(6).hex()
-h = base64.b64encode(hashlib.sha256((pw + salt).encode()).digest()).decode()
-with open(\"/etc/radicale/users\", \"w\") as f:
-    f.write(f\"{user}:{h}\\n\")
-print(\">>> User created\")
-"
+echo "${RADICALE_USER:-johnathan}:${RADICALE_PASS:-changeme}" > /etc/radicale/users
+echo ">>> User created"
 echo ">>> Starting Radicale on :5232"
 exec radicale --config /etc/radicale/config
