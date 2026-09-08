63239421e3f1dd6fbd03a0752792eaeca32c9a27
4z2svrpsyr-art <4z2svrpsyr@privaterelay.appleid.com>
2026-09-08 21:55:32 +0800

    fix: sha256_crypt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 python3 -c "
-from passlib.hash import bcrypt
+from passlib.hash import sha256_crypt
 import os
-h = bcrypt.hash(os.environ.get(\"RADICALE_PASS\", \"changeme\"))
+h = sha256_crypt.hash(os.environ.get(\"RADICALE_PASS\", \"changeme\"))
 with open(\"/etc/radicale/users\", \"w\") as f:
     f.write(f\"${os.environ.get(\"RADICALE_USER\", \"johnathan\")}:{h}\n\")
 print(\"User created\")
