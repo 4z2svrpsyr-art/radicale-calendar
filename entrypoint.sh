6d5ffe1be8409192d75f0a3fb10e075883f5cd7a
4z2svrpsyr-art <4z2svrpsyr@privaterelay.appleid.com>
2026-09-09 08:59:50 +0800

    fix: plain auth + sh-compatible syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 set -e
-python3 -c "
-from passlib.hash import sha256_crypt
-import os
-h = sha256_crypt.hash(os.environ.get(\"RADICALE_PASS\", \"changeme\"))
-with open(\"/etc/radicale/users\", \"w\") as f:
-    f.write(f\"${os.environ.get(\"RADICALE_USER\", \"johnathan\")}:{h}\n\")
-print(\">>> User created\")
-"
+echo "${RADICALE_USER:-johnathan}:${RADICALE_PASS:-changeme}" > /etc/radicale/users
+echo "User created"
 exec radicale --config /etc/radicale/config
